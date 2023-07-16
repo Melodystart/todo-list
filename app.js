54e8de9ed673dc5b6dbe76f5552872c2ec27497d
Melodystart <start99start@gmail.com>
2023-07-16 19:35:56 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const routes = require('./routes')
+
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+
 require('./config/mongoose')
 
 const app = express()
@@ -26,6 +30,9 @@ app.use(session({
 }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 
 app.use(routes)
 
