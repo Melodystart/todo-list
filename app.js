965ac44828433956eb39874733e92860fbeaf9b9
Melodystart <start99start@gmail.com>
2023-07-16 16:10:47 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
@@ -18,6 +19,11 @@ const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
