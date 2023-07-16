f8a617a7a9f932797554c20d7dcd8efcca55c8ad
Melodystart <start99start@gmail.com>
2023-07-16 14:28:21 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const routes = require('./routes')
+require('./config/mongoose')
+
+const app = express()
+mongoose.connect(process.env.MONGODB_URI)
+
+const PORT = process.env.PORT || 3000
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
